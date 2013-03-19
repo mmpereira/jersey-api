97eb76e520c4def78f0256338805427bd4f51616
Mario Pereira <mario.mpereira@gmail.com>
2013-03-19 15:04:59 +0100

    first commit

---
+++ src/main/java/com/genebio/nextprot/resource/PublicationResource.java
@@ -0,0 +1,34 @@
+package com.genebio.nextprot.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.genebio.nextprot.domain.Publication;
+import com.genebio.nextprot.service.PublicationService;
+
+@Service
+@Path("/publication")
+public class PublicationResource {
+
+	@Autowired
+	private PublicationService publicationService;
+	
+	@GET
+	@Produces(value = {"application/xml", "application/json"})
+	public Response hello() {
+		return Response.status(200).entity(new Publication()).build();
+	}
+	
+	@GET
+	@Path("/{pid}")
+	@Produces(value = {"application/xml", "application/json"})
+	public Response get(@PathParam("pid") String pid) {
+		return Response.status(200).entity(this.publicationService.getPublicationById(Integer.parseInt(pid))).build();
+	}
+}
