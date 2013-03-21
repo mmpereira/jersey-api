573767d7f9c61d6f74bb4fe01e66c1511e07af48
Daniel <ddtxra@gmail.com>
2013-03-21 11:23:46 +0100

    adding documentation

--- src/main/java/com/genebio/nextprot/resource/PublicationResource.java
+++ src/main/java/com/genebio/nextprot/resource/PublicationResource.java
@@ -13,22 +13,31 @@ import com.genebio.nextprot.domain.Publication;
 import com.genebio.nextprot.service.PublicationService;
 
 @Service
-@Path("/publication")
+@Path("publication")
 public class PublicationResource {
 
 	@Autowired
 	private PublicationService publicationService;
 	
+	// http://localhost:8080/jersey-api/publication/
 	@GET
 	@Produces(value = {"application/xml", "application/json"})
 	public Response hello() {
 		return Response.status(200).entity(new Publication()).build();
 	}
 	
+	// http://localhost:8080/jersey-api/publication/6634104/
 	@GET
-	@Path("/{pid}")
+	@Path("{pid}")
 	@Produces(value = {"application/xml", "application/json"})
 	public Response get(@PathParam("pid") String pid) {
 		return Response.status(200).entity(this.publicationService.getPublicationById(Integer.parseInt(pid))).build();
+	}
+	
+	@GET
+	@Path("/list/{title}")
+	@Produces(value = {"application/json"})
+	public Response getByTitle(@PathParam("title") String title) {
+		return Response.status(200).entity(this.publicationService.getPublicationByTitle("%"+title+"%")).build();
 	}
 }
